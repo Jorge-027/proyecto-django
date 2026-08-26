64fef37b1e61aba7af489385953439d2ac5cf571
Jorge-027 <jorge.sepulveda76@inacapmail.cl>
2026-08-26 15:10:36 -0400

    Agregado HttpResponse en vistas y rutas para app1/app2

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.urls import path
+from . import views
+from django.http import HttpResponse
 
+def vista1(request):
+    return HttpResponse("<h1>App1 - Vista1</h1><p>Hola desde App1/Vista1</p>")
+
+def vista2(request):
+    return HttpResponse("<h1>App1 - Vista2</h1><p>Hola desde App1/Vista2</p>")
+
+urlpatterns = [
+    path("v1/", views.vista1, name="app1-v1"),
+    path("v2/", views.vista2, name="app1-v2"),
+]
 # Create your views here.
