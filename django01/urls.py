e173c3cc62a209d19c3c7bc9d2ed9c681ddb11fd
Jorge-027 <jorge.sepulveda76@inacapmail.cl>
2026-08-27 12:30:29 -0400

    app inicio terminada

--- django01/urls.py
+++ django01/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("app1/", include("app1.urls")),
     path("app2/", include("app2.urls")),
+    path("", include("inicio.urls")),
 ]
 
 
